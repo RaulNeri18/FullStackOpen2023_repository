8ed3f75ed8999b87ac68c2c4798e1c496ddd0e93
Raul Neri <rnerip@gmail.com>
2023-11-12 18:51:26 -0500

    Uploading part3 exercises (3.11 - Deployment Frontend and Backend)

--- part3/models/person.js
+++ part3/models/person.js
@@ -15,8 +15,19 @@ mongoose.connect(url)
     })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+    name: {
+        type: String,
+        minLength: 3,
+        required: true
+    },
+    number: {
+        type: String,
+        validate: {
+            validator: v => /\d{2}-\d{7}/.test(v) || /\d{3}-\d{8}/.test(v),
+            message: props => `${props.value} is not a valid phone number`
+        },
+        required: true
+    }
 })
 
 personSchema.set('toJSON', {
